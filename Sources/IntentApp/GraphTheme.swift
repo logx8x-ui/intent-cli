fd63384fe1a0d7feaf483832bc4b248f81ca812a
Logan Mondi <loganmondi@users.noreply.github.com>
2026-07-16 13:38:25 +0800

    Increase adaptive backdrop intensity

--- Sources/IntentApp/GraphTheme.swift
+++ Sources/IntentApp/GraphTheme.swift
@@ -10,7 +10,7 @@ enum GraphTheme {
     }
 
     static func backdropTintOpacity(_ colorScheme: ColorScheme) -> Double {
-        colorScheme == .dark ? 0.26 : 0.38
+        colorScheme == .dark ? 0.15 : 0.27
     }
 
     static func chrome(_ colorScheme: ColorScheme) -> Color {
--- scripts/install.sh
+++ scripts/install.sh
@@ -46,9 +46,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.3.5</string>
+  <string>0.3.6</string>
   <key>CFBundleVersion</key>
-  <string>9</string>
+  <string>10</string>
   <key>LSMinimumSystemVersion</key>
   <string>13.0</string>
   <key>LSUIElement</key>
